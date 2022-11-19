b48bad7efe8d3f29a6a0f0084b3fd51d21f373fb
Tyler Horn <thorn@sterilissolutions.com>
2022-11-19 16:47:48 -0600

    update

--- plugins/aggregators/cyclestats/cyclestats.go
+++ plugins/aggregators/cyclestats/cyclestats.go
@@ -1,255 +1,107 @@
 package cyclestats
 
 import (
-	"testing"
-	"time"
-
-	"github.com/stretchr/testify/require"
+	_ "embed"
 
 	"github.com/influxdata/telegraf"
-	"github.com/influxdata/telegraf/metric"
-	"github.com/influxdata/telegraf/testutil"
+	"github.com/influxdata/telegraf/plugins/aggregators"
 )
 
-func TestSimple(t *testing.T) {
-	plugin := &Merge{}
+var sampleConfig = `
+  ## Available tags to attach to metrics:
+  ## * id
+  ## * cycle
+  ## * device_config
+  ## * grind_cycle
+  ## * steam_cycle
+  ## * waste_type
+  ## * type
+  ## * start_time
+  ## * end_time
+  ## * completed
+  ## * successful
+  portal_tags = [ "id", "grind_cycle", "steam_cycle" ]
+`
 
-	err := plugin.Init()
-	require.NoError(t, err)
-
-	plugin.Add(
-		testutil.MustMetric(
-			"cpu",
-			map[string]string{
-				"cpu": "cpu0",
-			},
-			map[string]interface{}{
-				"time_idle": 42,
-			},
-			time.Unix(0, 0),
-		),
-	)
-	require.NoError(t, err)
-
-	plugin.Add(
-		testutil.MustMetric(
-			"cpu",
-			map[string]string{
-				"cpu": "cpu0",
-			},
-			map[string]interface{}{
-				"time_guest": 42,
-			},
-			time.Unix(0, 0),
-		),
-	)
-	require.NoError(t, err)
-
-	var acc testutil.Accumulator
-	plugin.Push(&acc)
-
-	expected := []telegraf.Metric{
-		testutil.MustMetric(
-			"cpu",
-			map[string]string{
-				"cpu": "cpu0",
-			},
-			map[string]interface{}{
-				"time_idle":  42,
-				"time_guest": 42,
-			},
-			time.Unix(0, 0),
-		),
-	}
-
-	testutil.RequireMetricsEqual(t, expected, acc.GetTelegrafMetrics())
+type CycleStats struct {
+    // caches for metric fields, names, and tags
+    fieldCache map[uint64]map[string]float64
+    nameCache  map[uint64]string
+    tagCache   map[uint64]map[string]string
 }
 
-func TestNanosecondPrecision(t *testing.T) {
-	plugin := &Merge{}
-
-	err := plugin.Init()
-	require.NoError(t, err)
-
-	plugin.Add(
-		testutil.MustMetric(
-			"cpu",
-			map[string]string{
-				"cpu": "cpu0",
-			},
-			map[string]interface{}{
-				"time_idle": 42,
-			},
-			time.Unix(0, 1),
-		),
-	)
-	require.NoError(t, err)
-
-	plugin.Add(
-		testutil.MustMetric(
-			"cpu",
-			map[string]string{
-				"cpu": "cpu0",
-			},
-			map[string]interface{}{
-				"time_guest": 42,
-			},
-			time.Unix(0, 1),
-		),
-	)
-	require.NoError(t, err)
-
-	var acc testutil.Accumulator
-	acc.SetPrecision(time.Second)
-	plugin.Push(&acc)
-
-	expected := []telegraf.Metric{
-		testutil.MustMetric(
-			"cpu",
-			map[string]string{
-				"cpu": "cpu0",
-			},
-			map[string]interface{}{
-				"time_idle":  42,
-				"time_guest": 42,
-			},
-			time.Unix(0, 1),
-		),
-	}
-
-	testutil.RequireMetricsEqual(t, expected, acc.GetTelegrafMetrics())
+func NewCycleStats() telegraf.Aggregator {
+    m := &CycleStats{}
+    m.Reset()
+    return m
 }
 
-func TestReset(t *testing.T) {
-	plugin := &Merge{}
-
-	err := plugin.Init()
-	require.NoError(t, err)
-
-	plugin.Add(
-		testutil.MustMetric(
-			"cpu",
-			map[string]string{
-				"cpu": "cpu0",
-			},
-			map[string]interface{}{
-				"time_idle": 42,
-			},
-			time.Unix(0, 0),
-		),
-	)
-	require.NoError(t, err)
-
-	var acc testutil.Accumulator
-	plugin.Push(&acc)
-
-	plugin.Reset()
-
-	plugin.Add(
-		testutil.MustMetric(
-			"cpu",
-			map[string]string{
-				"cpu": "cpu0",
-			},
-			map[string]interface{}{
-				"time_guest": 42,
-			},
-			time.Unix(0, 0),
-		),
-	)
-	require.NoError(t, err)
-
-	plugin.Push(&acc)
-
-	expected := []telegraf.Metric{
-		testutil.MustMetric(
-			"cpu",
-			map[string]string{
-				"cpu": "cpu0",
-			},
-			map[string]interface{}{
-				"time_idle": 42,
-			},
-			time.Unix(0, 0),
-		),
-		testutil.MustMetric(
-			"cpu",
-			map[string]string{
-				"cpu": "cpu0",
-			},
-			map[string]interface{}{
-				"time_guest": 42,
-			},
-			time.Unix(0, 0),
-		),
-	}
-
-	testutil.RequireMetricsEqual(t, expected, acc.GetTelegrafMetrics())
+func (*CycleStats) SampleConfig() string {
+    return sampleConfig
 }
 
-var m1 = metric.New(
-	"mymetric",
-	map[string]string{
-		"host":        "host.example.com",
-		"mykey":       "myvalue",
-		"another key": "another value",
-	},
-	map[string]interface{}{
-		"f1": 1,
-		"f2": 2,
-		"f3": 3,
-		"f4": 4,
-		"f5": 5,
-		"f6": 6,
-		"f7": 7,
-		"f8": 8,
-	},
-	time.Now(),
-)
-var m2 = metric.New(
-	"mymetric",
-	map[string]string{
-		"host":        "host.example.com",
-		"mykey":       "myvalue",
-		"another key": "another value",
-	},
-	map[string]interface{}{
-		"f8":  8,
-		"f9":  9,
-		"f10": 10,
-		"f11": 11,
-		"f12": 12,
-		"f13": 13,
-		"f14": 14,
-		"f15": 15,
-		"f16": 16,
-	},
-	m1.Time(),
-)
-
-func BenchmarkMergeOne(b *testing.B) {
-	var merger Merge
-	err := merger.Init()
-	require.NoError(b, err)
-	var acc testutil.NopAccumulator
-
-	for n := 0; n < b.N; n++ {
-		merger.Reset()
-		merger.Add(m1)
-		merger.Push(&acc)
-	}
+func (m *CycleStats) Init() error {
+    return nil
 }
 
-func BenchmarkMergeTwo(b *testing.B) {
-	var merger Merge
-	err := merger.Init()
-	require.NoError(b, err)
-	var acc testutil.NopAccumulator
+func (m *CycleStats) Add(in telegraf.Metric) {
+    id := in.HashID()
+    if _, ok := m.nameCache[id]; !ok {
+        // hit an uncached metric, create caches for first time:
+        m.nameCache[id] = in.Name()
+        m.tagCache[id] = in.Tags()
+        m.fieldCache[id] = make(map[string]float64)
+        for k, v := range in.Fields() {
+            if fv, ok := convert(v); ok {
+                m.fieldCache[id][k] = fv
+            }
+        }
+    } else {
+        for k, v := range in.Fields() {
+            if fv, ok := convert(v); ok {
+                if _, ok := m.fieldCache[id][k]; !ok {
+                    // hit an uncached field of a cached metric
+                    m.fieldCache[id][k] = fv
+                    continue
+                }
+                if fv < m.fieldCache[id][k] {
+                    // set new minimum
+                    m.fieldCache[id][k] = fv
+                }
+            }
+        }
+    }
+}
 
-	for n := 0; n < b.N; n++ {
-		merger.Reset()
-		merger.Add(m1)
-		merger.Add(m2)
-		merger.Push(&acc)
-	}
+func (m *CycleStats) Push(acc telegraf.Accumulator) {
+    for id, _ := range m.nameCache {
+        fields := map[string]interface{}{}
+        for k, v := range m.fieldCache[id] {
+            fields[k+"_min"] = v
+        }
+        acc.AddFields(m.nameCache[id], fields, m.tagCache[id])
+    }
+}
+
+func (m *CycleStats) Reset() {
+    m.fieldCache = make(map[uint64]map[string]float64)
+    m.nameCache = make(map[uint64]string)
+    m.tagCache = make(map[uint64]map[string]string)
+}
+
+func convert(in interface{}) (float64, bool) {
+    switch v := in.(type) {
+    case float64:
+        return v, true
+    case int64:
+        return float64(v), true
+    default:
+        return 0, false
+    }
+}
+
+func init() {
+    aggregators.Add("min", func() telegraf.Aggregator {
+        return NewCycleStats()
+    })
 }
